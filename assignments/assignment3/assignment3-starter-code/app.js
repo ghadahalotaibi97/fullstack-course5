caebfdd41d78b07ad9dfe6e22777bfe9eeca1735
Ghadah Alotaibi <75266002+ghadahalotaibi97@users.noreply.github.com>
2023-12-27 20:04:49 +0300

    Update app.js

--- assignments/assignment3/assignment3-starter-code/app.js
+++ assignments/assignment3/assignment3-starter-code/app.js
@@ -1,41 +1,48 @@
 (function () {
-  'use strict';
+    'use strict';
 
-  angular.module('NarrowItDownApp', [])
-    .controller('NarrowItDownController', NarrowItDownController)
-    .directive('foundItems', FoundItemsDirective);
+    angular.module('NarrowItDownApp', [])
+        .controller('NarrowItDownController', NarrowItDownController)
+        .directive('foundItems', FoundItemsDirective);
 
-  NarrowItDownController.$inject = ['$http'];
-  function NarrowItDownController($http) {
-    var narrowCtrl = this;
-    narrowCtrl.searchTerm = '';
-    narrowCtrl.foundItems = [];
-
-    narrowCtrl.narrowItDown = function () {
-      if (narrowCtrl.searchTerm.trim() === '') {
+    NarrowItDownController.$inject = ['$http'];
+    function NarrowItDownController($http) {
+        var narrowCtrl = this;
+        narrowCtrl.searchTerm = '';
         narrowCtrl.foundItems = [];
-        return;
-      }
 
-      // Implement the logic to fetch and filter menu items from the server
-      // Update narrowCtrl.foundItems with the filtered items
-    };
+        narrowCtrl.narrowItDown = function () {
+            if (narrowCtrl.searchTerm.trim() === '') {
+                narrowCtrl.foundItems = [];
+                return;
+            }
 
-    narrowCtrl.removeItem = function (index) {
-      narrowCtrl.foundItems.splice(index, 1);
-    };
-  }
+            // Simulating data retrieval from the server
+            var mockMenuItems = [
+                {name: 'Dish 1', short_name: 'D1', description: 'Delicious dish'},
+                {name: 'Dish 2', short_name: 'D2', description: 'Tasty option'},
+                {name: 'Dish 3', short_name: 'D3', description: 'Exquisite food'},
+            ];
 
-  function FoundItemsDirective() {
-    var ddo = {
-      templateUrl: 'foundItems.html',
-      scope: {
-        foundItems: '<',
-        onRemove: '&'
-      }
-    };
+            narrowCtrl.foundItems = mockMenuItems.filter(function (item) {
+                return item.description.toLowerCase().includes(narrowCtrl.searchTerm.toLowerCase());
+            });
+        };
 
-    return ddo;
-  }
+        narrowCtrl.removeItem = function (index) {
+            narrowCtrl.foundItems.splice(index, 1);
+        };
+    }
 
+    function FoundItemsDirective() {
+        var ddo = {
+            templateUrl: 'foundItems.html',
+            scope: {
+                foundItems: '<',
+                onRemove: '&'
+            }
+        };
+
+        return ddo;
+    }
 })();
