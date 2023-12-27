e08fdea97276d9c64cc0bb0cc516f1bd76b4f70e
Ghadah Alotaibi <75266002+ghadahalotaibi97@users.noreply.github.com>
2023-12-27 19:57:24 +0300

    Create app.js

---
+++ assignments/assignment3/assignment3-starter-code/app.js
@@ -0,0 +1,41 @@
+(function () {
+  'use strict';
+
+  angular.module('NarrowItDownApp', [])
+    .controller('NarrowItDownController', NarrowItDownController)
+    .directive('foundItems', FoundItemsDirective);
+
+  NarrowItDownController.$inject = ['$http'];
+  function NarrowItDownController($http) {
+    var narrowCtrl = this;
+    narrowCtrl.searchTerm = '';
+    narrowCtrl.foundItems = [];
+
+    narrowCtrl.narrowItDown = function () {
+      if (narrowCtrl.searchTerm.trim() === '') {
+        narrowCtrl.foundItems = [];
+        return;
+      }
+
+      // Implement the logic to fetch and filter menu items from the server
+      // Update narrowCtrl.foundItems with the filtered items
+    };
+
+    narrowCtrl.removeItem = function (index) {
+      narrowCtrl.foundItems.splice(index, 1);
+    };
+  }
+
+  function FoundItemsDirective() {
+    var ddo = {
+      templateUrl: 'foundItems.html',
+      scope: {
+        foundItems: '<',
+        onRemove: '&'
+      }
+    };
+
+    return ddo;
+  }
+
+})();
