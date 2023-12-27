5190dfc743dea47d945a40e10afafa644d51245e
Ghadah Alotaibi <75266002+ghadahalotaibi97@users.noreply.github.com>
2023-12-27 21:16:13 +0300

    Create app.js

---
+++ assignments/assignment5/app.js
@@ -0,0 +1,5 @@
+// app.js
+var app = angular.module('yourAppName', ['ngRoute']);
+
+// Configure routes, controllers, and services
+// ... (add your route configurations, controllers, and services) ...
